451f2cdc976c7e073fd14380b1937b7ef9390c58
gvaduha <gvaduha@gmail.com>
2019-11-28 15:08:57 +0700

    Create git-changeauthor.sh

---
+++ bash/git-changeauthor.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+CORRECT_NAME="gvaduha"
+CORRECT_EMAIL="gvaduha@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" != "$CORRECT_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" != "$CORRECT_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
+echo "DO: git push --force --tags origin 'refs/heads/*'"
