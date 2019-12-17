68187912b3ea5cf1688dbcd966cd5aa7978e09ef
gvaduha <gvaduha@gmail.com>
2019-12-17 11:48:12 +0700

    Update log-parse-match-events.sh

--- bash/log-parse-match-events.sh
+++ bash/log-parse-match-events.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
 set LOG-FILE-NAME=$1
-set TMP-CSV-NAME=$2
+set TMP-CSV-NAME=log-tmp.csv #$2
 
-grep -i "ing to elvis" LOG-FILE-NAME sed 's/-/:/gp' | sed -rn 's/([0-9 \:]+)\..*room=([0-9]+).*pot=([0-9]+).*new_val=([0-1]).*/\1,\2,\3,\4/p' > TMP-CSV-NAME
+grep -i "ing to elv" LOG-FILE-NAME | sed 's/-/:/gp' | sed -rn 's/([0-9 \:]+)\..*room=([0-9]+).*pot=([0-9]+).*new_val=([0-1]).*/\1,\2,\3,\4/p' > TMP-CSV-NAME
+
+declare -A EVENTMAP
+
+while IFS=',' read -ra EVENT; do
+     echo ${EVENT[1]}
+done < TMP-CSV-NAME 
