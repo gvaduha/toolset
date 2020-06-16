6f96a32c525be315c515f566440a75caba1f5977
gvaduha <gvaduha@gmail.com>
2020-06-16 13:36:23 +0700

    Update ora-delete-all-tables.sql

--- sql/ora-delete-all-tables.sql
+++ sql/ora-delete-all-tables.sql
@@ -1,12 +1,23 @@
 begin
-    for cur in (select table_name from user_tables)
+    for tcur in (select table_name from user_tables)
     loop
         begin
-            dbms_output.put_line('Dropping ' || cur.table_name);
-            execute immediate 'drop table ' || cur.table_name || ' cascade constraints;';
+            dbms_output.put_line('Dropping ' || tcur.table_name);
+            execute immediate 'drop table ' || tcur.table_name || ' cascade constraints';
         exception
             when others then
-                dbms_output.put_line('Failed to drop ' || cur.table_name);
+                dbms_output.put_line('Failed to drop ' || tcur.table_name);
+        end;
+    end loop;
+
+    for scur in (select sequence_name from user_sequences)
+    loop
+        begin
+            dbms_output.put_line('Dropping ' || scur.sequence_name);
+            execute immediate 'drop sequence ' || scur.sequence_name;
+        exception
+            when others then
+                dbms_output.put_line('Failed to drop ' || scur.sequence_name);
         end;
     end loop;
 end;
