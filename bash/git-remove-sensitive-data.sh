5f82c1ac512eb13c817327d26f87549a1f1123e4
gvaduha <gvaduha@gmail.com>
2020-08-27 15:27:15 +0700

    Create git-remove-sensitive-data.sh

---
+++ bash/git-remove-sensitive-data.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+git filter-branch --force --index-filter \
+  "git rm --cached --ignore-unmatch PATH-TO-YOUR-FILE-WITH-SENSITIVE-DATA" \
+  --prune-empty --tag-name-filter cat -- --all
+  
+git push origin --force --all
