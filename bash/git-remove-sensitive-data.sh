17edb94e3152df0987edd418310f9c7b1914a4d9
gvaduha <gvaduha@gmail.com>
2020-08-27 15:51:13 +0700

    Update git-remove-sensitive-data.sh

--- bash/git-remove-sensitive-data.sh
+++ bash/git-remove-sensitive-data.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
+# https://docs.github.com/en/github/authenticating-to-github/removing-sensitive-data-from-a-repository
+
 git filter-branch --force --index-filter \
   "git rm --cached --ignore-unmatch PATH-TO-YOUR-FILE-WITH-SENSITIVE-DATA" \
   --prune-empty --tag-name-filter cat -- --all
   
 git push origin --force --all
+
+#git for-each-ref --format="delete %(refname)" refs/original | git update-ref --stdin
+#git reflog expire --expire=now --all
+#git gc --prune=now
